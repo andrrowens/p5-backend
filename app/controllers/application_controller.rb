47a8b4a8a337c0fc5f45753eeb35dfc6dd821943
Andrew Owens <andrrowens@gmail.com>
2023-02-27 16:17:55 -0700

    auth set up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,33 +1,31 @@
 class ApplicationController < ActionController::API
+    
+    # skip_forgery_protection
 
-    class ApplicationController < ActionController::Base
-        # skip_forgery_protection
-        # include ActionController::Cookies
-        rescue_from ActiveRecord::RecordNotFound, with: :not_found
-        rescue_from ActiveRecord::RecordInvalid, with: :not_valid
+    include ActionController::Cookies
+    rescue_from ActiveRecord::RecordNotFound, with: :not_found
+    rescue_from ActiveRecord::RecordInvalid, with: :not_valid
     
-        # before_action :authenticated_user
+    before_action :authenticated_user
     
-        # def current_user
-        #     @user ||= User.find_by(id:session[:user_id])
-        # end
+    def current_user
+        @user ||= User.find_by(id:session[:user_id])
+    end
         
-        # def authenticated_user
-        #     puts "test"
-        #     render json: {errors: "Not Authorized"}, status: :unauthorized unless current_user
-        # end
+    def authenticated_user
+        puts "test"
+        render json: {errors: "Not Authorized"}, status: :unauthorized unless current_user
+    end
     
     
-        private
+    private
     
-        def not_found(exception)
-            render json: {error: "#{exception.model} not found"}, status: :not_found
-        end
+    def not_found(exception)
+        render json: {error: "#{exception.model} not found"}, status: :not_found
+    end
         
-        def not_valid(invalid)
-            render json: {error: invalid.record.errors.full_messages}, status: :unprocessable_entity
-        end
-    
+    def not_valid(invalid)
+        render json: {error: invalid.record.errors.full_messages}, status: :unprocessable_entity
     end
     
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,14 @@ Rails.application.routes.draw do
   resources :plants
   resources :listings
   resources :users
+
+
+  get '/authenticated_user', to: 'users#show'
+
+  post '/signup', to: 'users#create'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
