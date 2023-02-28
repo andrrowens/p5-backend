bcd728cec3a9c273c0655527d7024741a1dd5a69
Andrew Owens <andrrowens@gmail.com>
2023-02-28 13:20:00 -0700

    profile page add

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,11 @@ class UsersController < ApplicationController
     skip_before_action :authenticated_user, only: [:create]
 
 
+    def index 
+        users = User.all 
+        render json: users, status: :ok
+    end
+
     def show 
         # user = User.find(params[:id])
         render json: @user, status: :ok
